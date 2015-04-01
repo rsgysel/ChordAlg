5e7e26b4d985d09be5a8eb170eb5926412623596
Rob Gysel <rsgysel@gmail.com>
2015-03-31 23:30:49 -0700

    src/ deletion

--- src/unit_tests/dimacs_fr_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "graph_test.h"
-
-// A simple test on a graph with two disjoint cliques. Intended to test the file reader, sorting / no self-loops of neighborhoods.
-TEST_F(DimacsGraphTest, DimacsFileReaderTest) {
-    Read(graph_dir() + std::string("myciel3.dimacs"));
-    //G->PrettyPrint();
-    return;
-}
